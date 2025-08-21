a92ec34a5d52fd1dd0b130f51d676d760b142c88
MayTheSlay <2543986@students.wits.ac.za>
2025-08-21 23:38:00 +0200

    addedd rouge

---
+++ Code/metrics.py
@@ -0,0 +1,11 @@
+from evaluate import load
+
+rouge = load("rouge")
+
+# predictions =
+# references=
+
+# results = rouge.compute(predictions=predictions,
+#                          references=references,
+#                             tokenizer=lambda x: x.split())
+# print(results)
